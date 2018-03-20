cacaa8b1bec99be11ca2efcf5e1d5e4739ae9836
Nicole RAveszani <nraveszani@gmail.com>
2018-03-20 11:38:51 -0300

    Agrego selectionSort en una carpeta para los ordenamientos

---
+++ TP1/Ordenamientos/selectionSort.py
@@ -0,0 +1,14 @@
+def selectionSort(array):
+    n = len(array) - 1
+    while n > 0:
+        p = findMax(array, 0, n)
+        array[p], array[n] = array[n], array[p]
+        n -= 1
+
+
+def findMax(array, a, b):
+    maxPos = a
+    for i in range(a + 1, b + 1, 1):
+        if array[i] > array[maxPos]:
+            maxPos = i
+    return maxPos
