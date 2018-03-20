daafb7ec0311583f1c2a83982786f844b3bf2987
Nicole RAveszani <nraveszani@gmail.com>
2018-03-20 15:10:17 -0300

    Agrego insercion y su test

---
+++ TP1/Ordenamientos/insertionSort.py
@@ -0,0 +1,13 @@
+def insertionSort(array):
+    n = len(array) - 1
+    for i in range(0, n):
+        if array[i + 1] < array[i]:
+            relocate(array, i + 1)
+
+def relocate(array, p):
+    v = array[p]
+    i = p
+    while i > 0 and v < array[i - 1]:
+        array[i] = array[i - 1]
+        i -= 1
+    array[i] = v
