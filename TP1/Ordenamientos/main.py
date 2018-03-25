cfbfbba7d400ebbb45ac80db9ef70c3139a2a653
FdelMazo <federicodelmazo@hotmail.com>
2018-03-25 19:11:43 -0300

    Interfaz para ordenar sets

---
+++ TP1/Ordenamientos/main.py
@@ -0,0 +1,35 @@
+import argparse
+import time
+
+from generarArrayRandom import generarArrayRandom
+from heapSort import heapSort
+from mergeSort import mergeSort
+from quickSort import quickSort
+from insertionSort import insertionSort
+from selectionSort import selectionSort
+
+SORTS = [insertionSort, selectionSort, mergeSort, quickSort, heapSort]
+CANTIDAD_ELEMENTOS = [50, 100, 500, 1000, 2000, 3000, 4000, 5000, 7500, 10000]
+
+def setALista(nombre_archivo, cantidadElementos):
+	lista = []
+	with open(nombre_archivo) as archivo:
+		for linea, i  in zip(archivo,range(cantidadElementos)):
+			lista.append(linea.strip())
+	return lista
+
+def main():
+	parser = argparse.ArgumentParser()
+	parser.add_argument('set',help='Nombre de archivo de set', nargs='?', action = 'store', default = "sets/set1.txt")
+	args = parser.parse_args()
+
+	for elementos in CANTIDAD_ELEMENTOS:
+		print("\n\nComenzando ordenamientos sobre {} elementos\n".format(elementos))
+		for ordenamiento in SORTS:
+			print("Ejecutando {} con {} elementos del set '{}'".format(ordenamiento.__name__, elementos, args.set))
+			start_time = time.clock()
+			lista = setALista(args.set, elementos)
+			ordenamiento(lista)
+			print("Tiempo final de ejecucion: {} segundos \n".format(time.clock() - start_time))
+
+main()
--- TP1/Ordenamientos/mergeSort.py
+++ TP1/Ordenamientos/mergeSort.py
@@ -4,16 +4,16 @@ def mergeSort(array):
 	mitad1, mitad2 = array[:medio], array[medio:]
 	izq = mergeSort(mitad1)
 	der = mergeSort(mitad2)
-	return merge(izq, der)
-	
-def merge(izq, der):
+	return _mergeSort(izq, der)
+
+def _mergeSort(izq, der):
 	resultado = []
 	i, j = 0,0
 	while i<len(izq) and j<len(der):
 		if izq[i]<der[j]:
 			resultado.append(izq[i])
 			i+=1
-		else: 
+		else:
 			resultado.append(der[j])
 			j+=1
 	resultado.extend(izq[i:])
