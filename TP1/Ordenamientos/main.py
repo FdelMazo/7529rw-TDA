c5b0c02ed7e46d01e22aab37957552d12e5a04e2
juankristal <juankristal@hotmail.com>
2018-03-28 16:31:36 -0300

    Automatizado del main

--- TP1/Ordenamientos/main.py
+++ TP1/Ordenamientos/main.py
@@ -1,5 +1,6 @@
 import argparse
 import time
+import os
 
 from heapSort import heapSort
 from mergeSort import mergeSort
@@ -17,18 +18,25 @@ def setALista(nombre_archivo, cantidadElementos):
 			lista.append(linea.strip())
 	return lista
 
-def main():
-	parser = argparse.ArgumentParser()
-	parser.add_argument('set',help='Nombre de archivo de set', nargs='?', action = 'store', default = "sets/set1.txt")
-	args = parser.parse_args()
+def rutasPorPalabra(palabra):
+	'''Recibe una palabra por parametro y devuelve las rutas (en una lista) de los archivos cuyo nombre contenga a dicha palabra '''
+	lista_rutas = []
+	for origen, carpetas, contenido in os.walk('.'):
+		for archivo in contenido:
+			if palabra in archivo:
+				lista_rutas.append((os.path.join(origen, archivo)))
+	return lista_rutas
 
-	for elementos in CANTIDAD_ELEMENTOS:
-		print("\n\nComenzando ordenamientos sobre {} elementos\n".format(elementos))
-		for ordenamiento in SORTS:
-			print("Ejecutando {} con {} elementos del set '{}'".format(ordenamiento.__name__, elementos, args.set))
-			start_time = time.process_time()
-			lista = setALista(args.set, elementos)
-			ordenamiento(lista)
-			print("Tiempo final de ejecucion: {} segundos \n".format(time.process_time() - start_time))
+def main():
+	lista_rutas = rutasPorPalabra("set")
+	for ruta_set in lista_rutas:
+		for elementos in CANTIDAD_ELEMENTOS:
+			print("\n\nComenzando ordenamientos sobre {} elementos\n".format(elementos))
+			for ordenamiento in SORTS:
+				print("Ejecutando {} con {} elementos del set '{}'".format(ordenamiento.__name__, elementos, ruta_set))
+				start_time = time.process_time()
+				lista = setALista(ruta_set, elementos)
+				ordenamiento(lista)
+				print("Tiempo final de ejecucion: {} segundos \n".format(time.process_time() - start_time))
 
 main()
