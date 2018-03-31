729f9821ee48fdbf49572dbc3195f2ffc5ed59f6
Esteban Djeordjian <edjeordjian@fi.uba.ar>
2018-03-31 00:37:11 -0300

    Generador documentado

--- TP1/Ordenamientos/sets/generarSetRandom.py
+++ TP1/Ordenamientos/sets/generarSetRandom.py
@@ -1,10 +1,19 @@
 import random
 
-def generarSet(nombre, cantidad):
-    arreglo =  random.sample(range(1,1000000), cantidad)
-    archivo = open(nombre + ".txt","w")
+def generarSet(nombre, cotaInf, cotaSup, cantidad):
+    arreglo =  random.sample(range(cotaInf,cotaSup), cantidad)
+    archivo = open(nombre,"w")
     for i in range(cantidad):
             archivo.write(str(arreglo[i]) + "\n")
     archivo.close()
 
-#generarSet("set1",10000)
+generarSet("set1.txt", 1, 1000000, 10000)
+generarSet("set2.txt", 1, 1000000, 10000)
+generarSet("set3.txt", 1, 1000000, 10000)
+generarSet("set4.txt", 1, 1000000, 10000)
+generarSet("set5.txt", 1, 1000000, 10000)
+generarSet("set6.txt", 1, 1000000, 10000)
+generarSet("set7.txt", 1, 1000000, 10000)
+generarSet("set8.txt", 1, 1000000, 10000)
+generarSet("set9.txt", 1, 1000000, 10000)
+generarSet("set10.txt", 1, 1000000, 10000)
