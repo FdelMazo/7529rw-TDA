0e54e169864c45d6ac76f68f356a6c588f91e327
FdelMazo <federicodelmazo@hotmail.com>
2018-04-08 16:46:50 -0300

    Informe, feedback

--- TP1/Ordenamientos/mergeSort.py
+++ TP1/Ordenamientos/mergeSort.py
@@ -1,10 +1,10 @@
 def mergeSort(array):
-        l = len(array)
-        if l < 2: return array
-        izq, der = mergeSort(array[:l//2]), mergeSort(array[l//2:])
-        resultado = []
-        while izq and der:
-                if izq[0] < der[0]: resultado.append(izq.pop(0))
-                else: resultado.append(der.pop(0))
-        resultado.extend(izq) if izq else resultado.extend(der)
-        return resultado
+    l = len(array)
+    if l < 2: return array
+    izq, der = mergeSort(array[:l//2]), mergeSort(array[l//2:])
+    resultado = []
+    while izq and der:
+        if izq[0] < der[0]: resultado.append(izq.pop(0))
+    else: resultado.append(der.pop(0))
+    resultado.extend(izq) if izq else resultado.extend(der)
+    return resultado
