da955663aaf4668d65aa39327d1cdbc3abe1d802
juankristal <juankristal@hotmail.com>
2018-04-08 02:11:03 -0300

    Upgrade al Selection

--- TP1/Ordenamientos/selectionSort.py
+++ TP1/Ordenamientos/selectionSort.py
@@ -1,10 +1,11 @@
 def selectionSort(array):
-	l = len(array)
-	for i in range(l, 0, -1):
-		m = array.index(obtenerMaxHasta(array, i))
-		array[m], array[i - 1] = array[i - 1], array[m]
-def obtenerMaxHasta(array, finArray):
-    max = array[0]
+    l = len(array)
+    for i in range(l, 0, -1):
+        max_pos = obtenerMaxPosHasta(array, i)
+        array[max_pos], array[i - 1] = array[i - 1], array[max_pos]
+
+def obtenerMaxPosHasta(array, finArray):
+    max_pos = 0
     for i in range(finArray):
-        if array[i] > max: max = array[i]
-    return max
+        if array[i] > array[max_pos]: max_pos = i
+    return max_pos
