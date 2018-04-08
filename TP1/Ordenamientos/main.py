58983b6bd32214296188cea2945ce42c58301ce9
FdelMazo <federicodelmazo@hotmail.com>
2018-04-08 00:37:53 -0300

    Refactor main

--- TP1/Ordenamientos/main.py
+++ TP1/Ordenamientos/main.py
@@ -1,81 +1,48 @@
 import argparse
 import time
-import sys
+import sys, os
 
 from heapSort import heapSort
 from mergeSort import mergeSort
 from quickSort import quickSort
 from insertionSort import insertionSort
 from selectionSort import selectionSort
-from manejoDeArchivos import setALista, rutasPorPalabra, rutaASet, rutasASet, exportCSV
+from manejoDeArchivos import *
 
-SORTS = [(insertionSort, "insertionSort"), (selectionSort, "selectionSort"), (mergeSort, "mergeSort"), (quickSort, "quickSort"), (heapSort, "heapSort")]
+SORTS = [insertionSort, selectionSort, mergeSort, quickSort, heapSort]
 CANTIDAD_ELEMENTOS = [50, 100, 500, 1000, 2000, 3000, 4000, 5000, 7500, 10000]
+DIRECTORIO_SETS = "sets/"
 
-def obtenerTiempoDeEjecucionDelOrdenamiento(ordenamiento, lista):
-    """Recibe la funcion del ordenamiento y una lista de ints.
-    Devuelve el tiempo de ejecucion en segundos de dicho ordenamiento sobre la lista."""
+def calcularTiempoEjecucion(ordenamiento, lista):
     start_time = time.process_time()
     ordenamiento(lista)
-    tiempo = time.process_time() - start_time
-    return tiempo
+    return time.process_time() - start_time
 
-def imprimirTiempoDeEjecucionDelOrdenamiento(setNombre, setDic, archivosDic):
-    """Recibe el nombre del set, el diccionario con los datos de todos los set y el diccionario
-    en el que se almacenan los tiempos de la cantidad de elementos con los que se corrieron cada ordenamiento."""
+def imprimirTiempoEjecucion(diccionarioSets, set, resultados):
     for elementos in CANTIDAD_ELEMENTOS:
-        print("\n\nComenzando ordenamientos sobre {} elementos\n".format(elementos))
+        print("Comenzando ordenamientos sobre {} elementos\n".format(elementos))
         for ordenamiento in SORTS:
-            print("Ejecutando {} con {} elementos del set '{}'".format(ordenamiento[0].__name__, elementos, setNombre))
-            lista = setDic[setNombre]
-            tiempo = obtenerTiempoDeEjecucionDelOrdenamiento(ordenamiento[0], lista[:elementos])
-            actualizarDiccionario(archivosDic, setNombre, elementos, ordenamiento[1], tiempo)
+            print("Ejecutando {} con {} elementos del set '{}'".format(ordenamiento.__name__, elementos, set))
+            lista = diccionarioSets[set][:elementos]
+            tiempo = calcularTiempoEjecucion(ordenamiento, lista)
+            actualizarDiccionarioResultados(resultados, set, elementos, ordenamiento.__name__, tiempo)
             print("Tiempo final de ejecucion: {} segundos \n".format(tiempo))
-
-def actualizarDiccionario(dic, setNombre, elementos, ordenamientoNombre, tiempo):
-    """Recibe: diccionario, el nombre del set, los elementos del set, el nombre del ordenamiento y el tiempo que tardo
-    ejecucion con el set y la cantidad nombrada anteriormente."""
-    dic[elementos] = dic.get(elementos, {})
-    dic[elementos][setNombre] = dic[elementos].get(setNombre, {})
-    dic[elementos][setNombre][ordenamientoNombre] = tiempo
-
-def crearDiccionarioDeSets(set):
-    """Recibe el nombre clave que representa a los sets.
-    Crea un diccionario donde la clave es el nombre del set y el valor la lista de numeros"""
-    dic = {}
-    listaRutas = rutasPorPalabra(set)
-    for setRuta in listaRutas:
-        nombreSet = rutaASet(setRuta)
-        arraySet = setALista(setRuta, 10000)
-        dic[nombreSet] = arraySet
-    return dic
+        print("\n")
 
 def main():
-    '''	parser = argparse.ArgumentParser()
-    parser.add_argument('set',help='Nombre de archivo de set', nargs='?', action = 'store', default = "sets/set1.txt")
-    args = parser.parse_args()
-    '''
-    """Como correr el main: python3 main.py 1.c o 1.f"""
     parser = argparse.ArgumentParser()
-    parser.add_argument('ejercicio', help='Punto del TP1 que se desea ejecutar. Puntos disponibles:'
-                                          '1.c (ejecutar tiempos de los ordenamientos),'
-                                          '1.f (ejecutar peores casos de los ordenamientos)', nargs='?', action='store', default=None)
+    parser.add_argument('caso', help='Caso a calcular (al azar, o peor caso)', nargs='?',
+        choices = ["Random", "Peor"], action='store', default="Random")
     args = parser.parse_args()
 
-    sys.setrecursionlimit(10000)
-    setDic = crearDiccionarioDeSets("set")
-    peoresCasosDic = crearDiccionarioDeSets("descendente")
-    setArchivosDic = {}
-    peoresCasosArchivosDic = {}
-    if str(args.ejercicio) == "1.c":
-        for setNombre in sorted(setDic):
-            imprimirTiempoDeEjecucionDelOrdenamiento(setNombre, setDic, setArchivosDic)
-    elif str(args.ejercicio) == "1.f":
-        for setNombre in sorted(peoresCasosDic):
-            imprimirTiempoDeEjecucionDelOrdenamiento(setNombre, peoresCasosDic, peoresCasosArchivosDic)
-    else:
-        print("WRONG!")
-    exportCSV(setArchivosDic, sorted(rutasPorPalabra("set")), "estadisticas")
-    exportCSV(peoresCasosArchivosDic, sorted(rutasPorPalabra("descendente")), "peoresCasos")
+    sys.setrecursionlimit(10000) # Necesario para el peor caso de quicksort
+
+    diccionarioSets, resultados = {}, {}
+    sets = sorted([s for s in os.listdir(DIRECTORIO_SETS) if args.caso in s])
+    for s in sets: diccionarioSets[s] = setALista(os.path.join(DIRECTORIO_SETS,s))
+
+    for set in sorted(diccionarioSets):
+        imprimirTiempoEjecucion(diccionarioSets, set, resultados)
+    exportCSV(resultados, sets)
 
 main()
--- TP1/Ordenamientos/manejoDeArchivos.py
+++ TP1/Ordenamientos/manejoDeArchivos.py
@@ -1,48 +1,25 @@
-import os
-
-def setALista(nombre_archivo, cantidadElementos):
+def setALista(nombre_archivo):
 	lista = []
 	with open(nombre_archivo) as archivo:
-		for linea, i  in zip(archivo,range(cantidadElementos)):
+		for linea  in archivo:
 			lista.append(linea.strip())
 	return lista
 
-def rutasPorPalabra(palabra):
-	'''Recibe una palabra por parametro y devuelve las rutas (en una lista) de los archivos cuyo nombre contenga a dicha palabra '''
-	lista_rutas = []
-	for origen, carpetas, contenido in os.walk('.'):
-		for archivo in contenido:
-			if palabra in archivo:
-				lista_rutas.append((os.path.join(origen, archivo)))
-	return lista_rutas
+def actualizarDiccionarioResultados(resultados, set, elementos, sort, tiempo):
+    resultados[elementos] = resultados.get(elementos, {})
+    resultados[elementos][set] = resultados[elementos].get(set, {})
+    resultados[elementos][set][sort] = tiempo
 
-def rutaASet(ruta):
-    '''Recibe una ruta de un archivo set y devuelve la cadena que representa a dicho set (ULTRA HARDCOREADO, hay que cambiarlo pero quiero visualizar el csv'''
-    lista = ruta.split("/")
-    nombre = lista[-1]
-    return nombre[:-4]
-
-def rutasASet(rutas):
-	aux = ""
-	for ruta in rutas:
-		aux+= ','
-		aux+= rutaASet(ruta)
-	return aux[1:]
-
-def exportCSV(diccionario, rutas, nombre):
-    keys = sorted(diccionario.keys())
-    cadena_aux = rutasASet(rutas)
-    nombre = str(nombre) + ".csv"
-    with open(os.path.join('.', nombre), "w") as archivo:
-        for key in keys:
-            archivo.write(str(key) + " Elementos," + cadena_aux + "\n")
-            aux = diccionario[key]
-            setsKeys = sorted(aux.keys())
-            for sort in sorted(aux[setsKeys[0]].keys()):
-                linea = sort + ","
-                for sets in setsKeys:
-                    linea+= str(aux[sets][sort])
-                    linea+= ","
-                archivo.write(linea[:-1] + "\n")
-            archivo.write("\n")
-### ultra hardcore todo esto, despues lo hago ver mas lindo (juani) ###
+def exportCSV(resultados, sets):
+	with open("Salida.csv", "w") as archivo:
+		for cantidadElementos in sorted(resultados.keys()):
+			archivo.write("{} Elementos,".format(str(cantidadElementos)) + ','.join(sets) + "\n")
+			diccionarioAux = resultados[cantidadElementos]
+			sets = sorted(diccionarioAux.keys())
+			sorts = sorted(diccionarioAux[sets[0]].keys())
+			for sort in sorts:
+				linea = sort + ','
+				for set in sets:
+					linea+= str(diccionarioAux[set][sort]) + ','
+				archivo.write(linea[:-1] + "\n")
+			archivo.write("\n")
