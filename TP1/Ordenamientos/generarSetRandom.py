21adce6c07e219dad0782bb001bf14a1674b1f28
juankristal <juankristal@hotmail.com>
2018-04-08 03:08:28 -0300

    Agrego peor caso de MergeSort y su set.

--- TP1/Ordenamientos/generarSetRandom.py
+++ TP1/Ordenamientos/generarSetRandom.py
@@ -1,4 +1,12 @@
 import random
+from antiMerge import antiMerge
+
+def generarAntiMerge(nombre, cantidad):
+        arreglo = antiMerge(sorted(random.sample(range(cantidad), cantidad))[::-1])
+        with open(nombre, "w") as archivo:
+                for numero in arreglo:
+                        archivo.write(str(numero) + "\n")
+        
 
 def generarSet(nombre, cotaInf, cotaSup, cantidad):
     arreglo =  random.sample(range(cotaInf,cotaSup), cantidad)
@@ -17,3 +25,4 @@ generarSet("SetRandom07.txt", 1, 1000000, 10000)
 generarSet("SetRandom08.txt", 1, 1000000, 10000)
 generarSet("SetRandom09.txt", 1, 1000000, 10000)
 generarSet("SetRandom10.txt", 1, 1000000, 10000)
+generarAntiMerge("SetAntiMerge.txt", 10000)
