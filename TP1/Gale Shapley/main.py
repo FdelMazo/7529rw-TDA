b4608fc0450da82b0962cc18e17da972046bf1c9
juankristal <juankristal@hotmail.com>
2018-04-08 23:13:38 -0300

    Documentadas las pruebas.

--- TP1/Gale Shapley/main.py
+++ TP1/Gale Shapley/main.py
@@ -3,21 +3,33 @@ from matchingGS import cargaDeArchivos
 from Jugador import Jugador
 from Equipo import Equipo
 
-def testMatchingGS():
-    '''Prueba que ningún jugador este discontento con su equipo y que ese equipo lo hubiese preferido'''
+RUTA = "asignacion.txt"
+
+def testPreferenciasGS():
+    '''Prueba que dadas las asignaciones finales, ningún jugador este discontento con su equipo y que ninguno de sus equipos favoritos lo hubiese preferido'''
     jugadores, equipos = cargaDeArchivos(200,20,10)
-    matchingGS(jugadores, equipos, 200, "asignacion.txt")
+    matchingGS(jugadores, equipos, 200, RUTA)
+    with open(RUTA) as archivo:
+        for linea in archivo:
+            print(linea)
     for equipo in equipos.values():
         for jugador in equipo.getJugadores():
             descartados = jugador.getEquiposDescartados()
             for equipo_descartado in descartados:
                 assert(equipos[equipo_descartado].getPosicionFavoritoActual() <= equipos[equipo_descartado].getPreferencias().index(jugador.getNumero()))
-    print("OK")
+    print("OK\n")
 
-def testMatchingGS2():
-    ''''''
+def testInestabilidadesGS():
+    '''Prueba que nunca se dara en las asignaciones finales:
+    Equipo1 tiene a Jugador1 asignado
+    Equipo2 tiene a Jugador2 asignado
+    Equipo1 prefiere a Jugador2 antes que a Jugador1
+    Equipo2 prefiere a Jugador1 antes que a Jugador2'''
     jugadores, equipos = cargaDeArchivos(200,20,10)
-    matchingGS(jugadores, equipos, 200, "asignacion.txt")
+    matchingGS(jugadores, equipos, 200, RUTA)
+    with open(RUTA) as archivo:
+        for linea in archivo:
+            print(linea)
     for equipo_original in equipos.values():
         for jugador_original in equipo_original.getJugadores():
             descartados = jugador_original.getEquiposDescartados()
@@ -26,14 +38,14 @@ def testMatchingGS2():
                     condicion1 = equipo_original.getPosicionFavoritoActual() <= equipo_original.getPreferencias().index(jugador_externo.getNumero())
                     condicion2 = equipos[equipo_descartado].getPosicionFavoritoActual() <= equipos[equipo_descartado].getPreferencias().index(jugador_original.getNumero())
                     assert(condicion1 or condicion2)
-    print("OK")
+    print("OK\n")
 
 def main():
     ''' Llamada a la función de asignación.
     '''
     jugadores, equipos = cargaDeArchivos(200,20,10)
-    matchingGS(jugadores, equipos, 200, "asignacion.txt") #Cambiar a True si se quieren usar nuevos sets aleatorios
+    matchingGS(jugadores, equipos, 200, RUTA) #Cambiar a True si se quieren usar nuevos sets aleatorios
 
 main()
-testMatchingGS()
-testMatchingGS2()
+testPreferenciasGS()
+testInestabilidadesGS()
