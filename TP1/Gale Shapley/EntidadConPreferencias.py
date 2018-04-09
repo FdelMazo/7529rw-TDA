0fb0d97f1962a65eeefe3fde57df94fc752a21ac
Esteban Djeordjian <edjeordjian@fi.uba.ar>
2018-04-08 21:08:48 -0300

    Documentada la clase entidad con preferencias.

--- TP1/Gale Shapley/EntidadConPreferencias.py
+++ TP1/Gale Shapley/EntidadConPreferencias.py
@@ -10,19 +10,38 @@ class EntidadConPreferencias:
         self.posicionFavoritoActual = 0
 
     def setNumero(self, numero):
+        ''' Setea el número de entidad.
+        Pre: numero es un número natural.
+        Post: el número fue asignado.
+        '''
         self.numero = numero
 
     def setPreferencias(self, preferencias):
+        ''' Setea las preferencias de la entidad.
+        Pre: preferencias es una lista de números naturales.
+        Post: la lista fue asignada.
+        '''
         self.preferencias = preferencias
 
+    def setPosicionFavoritoActual(self, posicionFavoritoActual):
+        ''' Asigna la posición en la lista de preferencias de la entidad.
+        '''
+        self.posicionFavoritoActual = posicionFavoritoActual
+
     def getNumero(self):
+        ''' Devuelve el número de entidad.
+        '''
         return self.numero
 
     def getPreferencias(self):
+        ''' Devuelve las preferencias de la entidad.
+        '''
         return self.preferencias
 
     def getPosicionFavoritoActual(self):
+        ''' Devuelve la posición de mayor preferencia
+        de la entidad dentro de su lista de preferencias.
+        Incluso si la lista está vacia, devuelve el valor
+        inicial asignado al inicializar la entidad.
+        '''
         return self.posicionFavoritoActual
-
-    def setPosicionFavoritoActual(self, posicionFavoritoActual):
-        self.posicionFavoritoActual = posicionFavoritoActual
