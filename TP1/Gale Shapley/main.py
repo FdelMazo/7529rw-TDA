7c4ec18031fee50cb09da59795c12a7682fe8b66
Esteban Djeordjian <edjeordjian@fi.uba.ar>
2018-04-08 21:25:44 -0300

    Mínima modificación en el main de gs

--- TP1/Gale Shapley/main.py
+++ TP1/Gale Shapley/main.py
@@ -1,18 +1,19 @@
 from matchingGS import matchingGS
-ARCHIVO  = "asignacion.txt"
 
 def testMatchingGS():
-    '''Prueba que ningun jugador este discontento con su equipo y que ese equipo lo hubiese preferido'''
-    jugadores, equipos = matchingGS(200, 20, 10, ARCHIVO, True)
+    '''Prueba que ningún jugador este discontento con su equipo y que ese equipo lo hubiese preferido'''
+    jugadores, equipos = matchingGS(200, 20, 10, "asignacion.txt", False)
     for equipo in equipos.values():
         for jugador in equipo.getJugadores():
             descartados = jugador.getEquiposDescartados()
             for equipo_descartado in descartados:
                 assert(equipos[equipo_descartado].getPosicionFavoritoActual() <= equipos[equipo_descartado].getPreferencias().index(jugador.getNumero()))
-    print("El matching es estable: OK")
+    print("OK")
 
 def main():
-    matchingGS(200, 20, 10, ARCHIVO, False) #Cambiar a True si se quieren usar sets aleatorios
+    ''' Llamada a la función de asignación.
+    '''
+    matchingGS(200, 20, 10, "asignacion.txt", False) #Cambiar a True si se quieren usar nuevos sets aleatorios
 
 main()
-testMatchingGS()
+#testMatchingGS()
