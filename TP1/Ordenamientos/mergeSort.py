76e6fb3b481d51fa1cc9b8b8794a523e0b6f6411
juankristal <juankristal@hotmail.com>
2018-04-20 23:21:32 -0300

    No more pop en mergeSort

--- TP1/Ordenamientos/mergeSort.py
+++ TP1/Ordenamientos/mergeSort.py
@@ -3,8 +3,14 @@ def mergeSort(array):
         if l < 2: return array
         izq, der = mergeSort(array[:l//2]), mergeSort(array[l//2:])
         resultado = []
-        while izq and der:
-                if izq[0] < der[0]: resultado.append(izq.pop(0))
-                else: resultado.append(der.pop(0))
-        resultado.extend(izq) if izq else resultado.extend(der)
+        cont_izq = 0
+        cont_der = 0
+        while cont_izq < len(izq) and cont_der < len(der):
+                if izq[cont_izq] < der[cont_der]: 
+                        resultado.append(izq[cont_izq])
+                        cont_izq+=1
+                else: 
+                        resultado.append(der[cont_der])
+                        cont_der+=1
+        resultado.extend(izq[cont_izq:]) if cont_der == len(der) else resultado.extend(der[cont_der:])
         return resultado
