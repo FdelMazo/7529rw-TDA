1d2d6b1106433079ef00d05acc243038b0fe10ca
juankristal <juankristal@hotmail.com>
2018-04-26 19:52:07 -0300

    Solucion naive de la rotacion de cadenas.

---
+++ TP2/StringRotation/rotacion.py
@@ -0,0 +1,10 @@
+def rotar_cadena(cadena, veces = 1):
+	'''Recibe una cadena por parametro y la cantidad de veces que se la rotara. Devuelve una nueva cadena rotada veces cantidad de veces.'''
+	return cadena[-veces:] + cadena[:-veces]
+
+def es_rotacion_ciclica(cadena1, cadena2):
+	'''Recibe dos cadenas pro parametro y devuelve si la segunda es una rotacion ciclica de la primera.'''
+	for x in range(len(cadena1)):
+		if rotar_cadena(cadena2, x) == cadena1:
+			return True
+	return False
