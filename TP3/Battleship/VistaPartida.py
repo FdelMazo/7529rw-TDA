fa82db00fd922203b75af0b9b7f7fb9eea2ddf8d
FdelMazo <federicodelmazo@hotmail.com>
2018-06-10 14:46:31 -0300

    impresion para distintos digitos de barcos

--- TP3/Battleship/VistaPartida.py
+++ TP3/Battleship/VistaPartida.py
@@ -13,22 +13,26 @@ class VistaPartida():
 		self.terminal.clear()
 
 	def imprimirMapa(self):
+		digitos = 0
+		for fila in self.partida.matriz:
+			digitosFila = len(str(max(fila)))
+			digitos = digitosFila if digitosFila>digitos else digitos
 		for columna, fila in enumerate(self.partida.matriz):
-			self.imprimirFila(fila, columna)
+			self.imprimirFila(fila, columna,digitos)
 		print("*******************************\n")
 		self.terminal.clear()
 
-	def imprimirFila(self, fila, y):
+	def imprimirFila(self, fila, y, digitos):
 		linea = ""
 		barco = self.partida.getBarcos()[y]
 		xBarco = barco.getPosicion()[0]
 		for x, n in enumerate(fila):
 			if x == xBarco and not barco.estaDerribado():
-				num = "<{}>".format(str(n).center(3))
+				num = "<{}>".format(str(n).rjust(digitos))
 				celda = self.terminal.pretty_string(num, 'OK')
 				celda += "|"
 			else:
-				num = "{}".format(n).center(5)
+				num = "{}".format(n).center(digitos+2)
 				celda = "{}|".format(num)
 			linea += celda
 		if barco.estaDerribado():
