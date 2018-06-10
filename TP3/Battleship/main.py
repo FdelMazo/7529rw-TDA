8c9722b6cdca924fe986c66e1b0294896e53e560
FdelMazo <federicodelmazo@hotmail.com>
2018-06-10 15:01:14 -0300

    Archivo por parametro

--- TP3/Battleship/main.py
+++ TP3/Battleship/main.py
@@ -2,7 +2,6 @@ import CrearGrilla
 from Juego import Juego
 from VistaJuego import VistaJuego
 from Greedo import Greedo
-from GreedoSmart import GreedoSmart
 from Dyno import Dyno
 import argparse
 
@@ -10,8 +9,8 @@ import argparse
 # De querer usar otra grilla, se recomienda crearla con CrearGrilla.py y pasarle los parametros a ese archivo
 DEFAULT_GRILLA = [5, 10, (500, 1000), 300]
 
-DEFAULT_LANZADERAS = 2 # Se puede pisar con argumentos al ejecutar el programa
-ARCHIVO = 'grilla.coords'
+DEFAULT_LANZADERAS = 2
+DEFAULT_ARCHIVO = 'grilla.coords'
 
 
 def jugar(partida, vista,jugador):
@@ -37,12 +36,13 @@ def main():
 	parser.add_argument('-y', '--no-input', help='Deshabilitar el input del usuario', action='store_true')
 	parser.add_argument('-o', '--sobreescribir', help='Sobreescribir el mapa al comenzar la ejecución', action='store_true')
 	parser.add_argument('-l', '--lanzaderas', help='Elegir la cantidad de lanzaderas a usar', type=int, action='store', default=DEFAULT_LANZADERAS)
+	parser.add_argument('-f', '--archivo', help='Elegir el archivo de mapa a usar', action='store', default=DEFAULT_ARCHIVO)
 	args = parser.parse_args()
 
-	archivo = ARCHIVO
+	archivo = args.archivo
 
 	if CrearGrilla.archivoNoExiste(archivo) or args.sobreescribir:
-		archivo = CrearGrilla.crearArchivo(ARCHIVO, DEFAULT_GRILLA)
+		archivo = CrearGrilla.crearArchivo(args.archivo, DEFAULT_GRILLA)
 
 	try:
 		CrearGrilla.verificarGrilla(archivo)
@@ -58,7 +58,7 @@ def main():
 	vistaJuego = VistaJuego(juego, args.no_input)
 	vistaJuego.titulo()
 
-	jugadores = [Greedo(), GreedoSmart()]
+	jugadores = [Greedo()]
 	for i,jugador in enumerate(jugadores):
 		juego.agregarJugador(jugador)
 		partida = juego.nuevaPartidaCon(jugador)
