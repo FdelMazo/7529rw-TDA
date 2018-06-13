11f0fbf1d64c241f84dc18f6a99a1546f9249070
FdelMazo <federicodelmazo@hotmail.com>
2018-06-12 23:10:15 -0300

    Agrego funcion para saber cuan dificil es matar a un barco

--- TP3/Battleship/Dyno.py
+++ TP3/Battleship/Dyno.py
@@ -1,5 +1,49 @@
 from Jugador import Jugador
 from copy import deepcopy
+from heapq import heappush, heappop
+import itertools
+
+def subsetMoreThan(lista, objetivo, cantidadLanzaderas, listaOriginal):
+	listaMultiplicada = [i * cantidadLanzaderas for i in lista]
+	if sum(listaMultiplicada[:-1]) >= objetivo:
+		return subsetMoreThan(lista[:-1], objetivo, cantidadLanzaderas,listaOriginal)
+	# Dirtiest hack in history: https://stackoverflow.com/a/34238688
+	for elem in listaMultiplicada[:]:
+		indice = listaMultiplicada.index(elem)
+		listaMultiplicada.remove(elem)
+		if sum(listaMultiplicada) >= objetivo:
+			lista.pop(indice)
+		else: listaMultiplicada.insert(indice, elem)
+	if sum(listaMultiplicada) < objetivo: return
+	resultado = []
+	for elem in lista:
+		indice = listaOriginal.index(elem)
+		if indice in resultado:
+			indice += listaOriginal[indice:].index(elem) +1
+		resultado.append(indice)
+	return tuple(resultado)
+
+def turnosDondePuedeMorir(fila, barco, cantidadLanzaderas):
+	combinacionesMatadorasBarco = set()
+	for j in range(len(fila)):
+		combinacion = subsetMoreThan(fila[j:], barco.getVida(), cantidadLanzaderas, fila)
+		if combinacion: combinacionesMatadorasBarco.add(combinacion)
+	return combinacionesMatadorasBarco
+
+def encontrarTodosLosTurnosDondeMuerenTodosLosBarcos(matriz, barcos, cantidadLanzaderas):
+	turnosParaTodos = {}
+	for y,fila in enumerate(matriz):
+		turnosDondeMuere = turnosDondePuedeMorir(fila, barcos[y], cantidadLanzaderas)
+		if turnosDondeMuere: turnosParaTodos[barcos[y]] = sorted(turnosDondeMuere)
+	return turnosParaTodos
+
+def ordenarPorBarcoMasDificilDeMatar(matriz, barcos, cantidadLanzaderas):
+	turnosParaTodos = encontrarTodosLosTurnosDondeMuerenTodosLosBarcos(matriz, barcos, cantidadLanzaderas)
+	heapBarcosDificiles = []
+	for barco in turnosParaTodos.keys():
+		cantPosiblesCombinaciones = len(turnosParaTodos[barco])
+		heappush(heapBarcosDificiles, (cantPosiblesCombinaciones, barco.getID()))
+	return heapBarcosDificiles
 
 
 class Dyno(Jugador):
@@ -10,79 +54,25 @@ class Dyno(Jugador):
 	def __init__(self):
 		super().__init__('Dyno')
 
-	def turnoDondeMuere(self, barco, matriz):
-		"""Devuelve la primera columna en el que se le puede ganar a un barco con la tupla (NroVuelta, Columna)
-		Ojo, ambos indices comienzan en 1 en vez de en 0, porque se refieren a vueltas y a columnas (no turnos)"""
-		numFila = barco.getID()
-		fila = matriz[numFila]
-		objetivo = barco.getVida()
-
-		vuelta = 1
-		i = 0
-		while objetivo > 0:
-			for i,elem in enumerate(fila,1):
-				objetivo -= elem
-				if objetivo <= 0: break
-			if objetivo<=0: break
-			vuelta+=1
-		return (vuelta, i)
-
-	def elegirTargetDelTurno(self, partida):
-		barcos, lanzaderas = partida.getBarcosVivos(), partida.getCantidadLanzaderas()
-		danioSegunBarco = []
-		for barco in reversed(barcos):
-			# Reversed para que sea estable el ordenamiento y si tiene dos barcos con la misma vida elija en orden
-			x, y = barco.getPosicion()
-			danioSegunBarco.append((barco, partida.getDanioCasillero(x, y)))
-		danioSegunBarco.sort(key=lambda x: x[1])
-		barcoActual = danioSegunBarco[-1]
-		vidaActual = barcoActual[0].getVida()
-		targets = []
-		for i in range(lanzaderas):
-			barco, danio = barcoActual
-			vidaActual -= danio
-			targets.append(barco.getID())
-			if vidaActual <=0:
-				danioSegunBarco.pop()
-				if not danioSegunBarco: break
-				barcoActual = danioSegunBarco[-1]
-				vidaActual = barcoActual[0].getVida()
-		targets += [None] * (lanzaderas - len(targets))
-		return targets
-
 	def elegirTargetsDeLaPartida(self, partidaOriginal):
-		simulacion = deepcopy(partidaOriginal)
-		targetsTotales = []
-		matriz = simulacion.getMatriz()
-
-		vuelta, ultimaColumna = max([self.turnoDondeMuere(b, matriz) for b in simulacion.getBarcos()]	)
-		cantidadColumnas = len(simulacion.getMatriz()[0])
-		cantidadBarcos = len(simulacion.getBarcos())
+		pass
 
 
-		cotaTotal = vuelta*cantidadColumnas*cantidadBarcos
-		#print(vuelta, cantidadColumnas,cotaTotal)
+if __name__=='__main__':
+	from Juego import Juego
+	from Partida import Partida
 
-		while not simulacion.terminada():
-			targets = self.elegirTargetDelTurno(simulacion)
-			simulacion.setTargetDelTurno(targets)
-			simulacion.jugarTurno()
-			targetsTotales.append(targets)
+	archivo = 'grilla.coords'
+	matrizTablero = Juego.ArchivoToMatriz(archivo)
+	barcos = Juego.ArchivoToBarcos(archivo)
+	cantidadLanzaderas = 1
 
-		return targetsTotales
-
-
-# if __name__=='__main__':
-# 	from Juego import Juego
-# 	from Partida import Partida
-#
-# 	archivo = 'grilla.coords'
-# 	matrizTablero = Juego.ArchivoToMatriz(archivo)
-# 	barcos = Juego.ArchivoToBarcos(archivo)
-# 	cantidadLanzaderas = 3
-#
-# 	juego = Juego(matrizTablero, barcos, cantidadLanzaderas)
-# 	jugador = Dyno()
-# 	partida = Partida(matrizTablero, barcos, cantidadLanzaderas, jugador)
-# 	partida.setPosicionesIniciales()
-# 	jugador.elegirTargetsDeLaPartida(partida)
+	juego = Juego(matrizTablero, barcos, cantidadLanzaderas)
+	jugador = Dyno()
+	partida = Partida(matrizTablero, barcos, cantidadLanzaderas, jugador)
+	partida.setPosicionesIniciales()
+	heap = ordenarPorBarcoMasDificilDeMatar(matrizTablero, barcos, cantidadLanzaderas)
+	while heap:
+		cantPosiblesCombinaciones, id = heappop(heap)
+		print("Hay {} chances de matar al {}".format(cantPosiblesCombinaciones, barcos[id]))
+	# jugador.elegirTargetsDeLaPartida(partida)
