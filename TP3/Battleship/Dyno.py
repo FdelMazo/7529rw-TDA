9cb3dc19ad99f7cc549dbf8fc7e1e280d5053c87
FdelMazo <federicodelmazo@hotmail.com>
2018-06-17 20:47:02 -0300

    Refactor dyno pt3

--- TP3/Battleship/Dyno.py
+++ TP3/Battleship/Dyno.py
@@ -23,21 +23,42 @@ class Dyno(Jugador):
 	def elegirTargetsDeLaPartida(self, partidaOriginal):
 		matriz, barcos, cantidadLanzaderas = partidaOriginal.getMatriz(), partidaOriginal.getBarcosVivos(), partidaOriginal.getCantidadLanzaderas()
 
-		turnosParaTodos = self.comoMatarBarcos(matriz, barcos)
-		heapDeBarcosDificiles = sortPorBarcoDificilDeMatar(turnosParaTodos)
-		_, barcoActualID = heappop(heapDeBarcosDificiles)
-		partidasPorBarco = self.combinacionesAPartidas(barcoActualID, turnosParaTodos[barcos[barcoActualID]],cantidadLanzaderas)
+		turnosParaTodos = {}
+		for id, fila in enumerate(matriz):
+			turnosDondeMuere = self.combinacionesMatadoras(fila, barcos[id].getVida())
+			if turnosDondeMuere: turnosParaTodos[barcos[id]] = sorted(turnosDondeMuere)
 
-		resultados = self.definirPartidaAJugar(heapDeBarcosDificiles, turnosParaTodos, partidasPorBarco, barcos,cantidadLanzaderas)
+		heapDeBarcosDificiles = self.sortPorBarcoDificilDeMatar(turnosParaTodos)
+		_, barcoMasDificil = heappop(heapDeBarcosDificiles)
+		partidasPrimerBarco = self.combinacionesAPartidas(barcoMasDificil, turnosParaTodos[barcos[barcoMasDificil]],cantidadLanzaderas)
+		partidaAJugar = self.definirPartidaAJugar(heapDeBarcosDificiles, turnosParaTodos, partidasPrimerBarco, barcos,cantidadLanzaderas)
 
-		partida, barcosRemanentes = self.jugarPartidaMatrizFinita(resultados, barcos)
-		if barcosRemanentes: partida = matarBarcosRemanentes(partida, barcosRemanentes, matriz, cantidadLanzaderas)
+		partida, barcosRemanentes = self.jugarPartidaMatrizFinita(partidaAJugar, barcos)
+		if barcosRemanentes: partida = self.matarBarcosRemanentes(partida, barcosRemanentes, matriz, cantidadLanzaderas)
 		return partida
+
+	def definirPartidaAJugar(self, heapDeBarcosDificiles, turnosParaTodos, partidasPrimerBarco, barcos, cantidadLanzaderas):
+		"""Recorre todos los barcos del mas dificil al mas facil de matar.
+		Para cada barco juega la partida anterior pero agregandole un barco
+		De todas las posibles partidas, se queda con la optima
+		Al final, devuelve la partida optima de todas las partidas optimas posibles
+		"""
+		partidas = partidasPrimerBarco
+		while heapDeBarcosDificiles:
+			_, proximoBarco = heappop(heapDeBarcosDificiles)
+			partidasProximoBarco = self.combinacionesAPartidas(proximoBarco, turnosParaTodos[barcos[proximoBarco]],cantidadLanzaderas)
+			for i, partida in enumerate(partidas):
+				partidasPosibles = []
+				for partidaActual in partidasProximoBarco:
+					partidaPosible = self.jugarPartidaNueva(partida, partidaActual, cantidadLanzaderas)
+					if not partidaPosible: continue
+					if partidaPosible not in partidasPosibles: partidasPosibles.append(partidaPosible)
+				if partidasPosibles: partidas[i] = self.partidaOptima(partidasPosibles)
+		return self.partidaOptima(partidas)
 
 	def partidaOptima(self, partidas):
 		"""La partida optima de una lista de partidas es la mas 'ajustada', es decir, la que mas dispara y gana lo antes posible
-		De esta forma se logra que no se extienda la partida dejandola llena de lanzaderas que no disparan
-		"""
+		De esta forma se logra que no se extienda la partida dejandola llena de lanzaderas que no disparan"""
 		atributoPartidas = []
 		for partida in partidas:
 			atributoPartidas.append((partida, self.contadorTargetsVacios(partida)))
@@ -49,7 +70,6 @@ class Dyno(Jugador):
 			if turno:
 				cantLanzaderas = len(turno)
 				break
-
 		contadorNones = 0
 		for turno in partida:
 			if turno:
@@ -57,17 +77,6 @@ class Dyno(Jugador):
 			else:
 				contadorNones += cantLanzaderas
 		return contadorNones
-
-	def combinacionesAPartidas(self, idBarco, combinaciones, cantidadLanzaderas):
-		"""Recibe una lista de turnos en los cuales disparar y los convierte en una partida valida"""
-		partidas = []
-		for combinacion in combinaciones:
-			partida = []
-			for turno in combinacion:
-				while len(partida) <= turno: partida += [None]
-				partida[turno] = [idBarco] + [None] * (cantidadLanzaderas - 1)
-			partidas.append(partida)
-		return partidas
 
 	def combinacionesMatadoras(self, lista, objetivo):
 		"""Recibe una lista de numeros y un objetivo
@@ -104,31 +113,20 @@ class Dyno(Jugador):
 			if listaIndices not in resultados: resultados.append(listaIndices)
 		return resultados
 
-	def comoMatarBarcos(self, matriz, barcos):
-		turnosParaTodos = {}
-		for y, fila in enumerate(matriz):
-			turnosDondeMuere = self.combinacionesMatadoras(fila, barcos[y].getVida())
-			if turnosDondeMuere: turnosParaTodos[barcos[y]] = sorted(turnosDondeMuere)
-		return turnosParaTodos
-
-	def definirPartidaAJugar(self, heapDeBarcosDificiles, turnosParaTodos, partidasPorBarco, barcos,cantidadLanzaderas):
-		resultados = partidasPorBarco
-		while heapDeBarcosDificiles:
-			_, barcoActualID = heappop(heapDeBarcosDificiles)
-			partidasBarcoActual = self.combinacionesAPartidas(barcoActualID, turnosParaTodos[barcos[barcoActualID]],cantidadLanzaderas)
-			for i, partida in enumerate(resultados):
-				partidasPosiblesAAppendear = []
-				for partidaActual in partidasBarcoActual:
-					partidaPosible = simularPartidaConPartidaParcial(partida, partidaActual, cantidadLanzaderas)
-					if partidaPosible and partidaPosible not in partidasPosiblesAAppendear: partidasPosiblesAAppendear.append(
-						partidaPosible)
-				if partidasPosiblesAAppendear: resultados[i] = self.partidaOptima(partidasPosiblesAAppendear)
-		return resultados
+	def combinacionesAPartidas(self, idBarco, combinaciones, cantidadLanzaderas):
+		"""Recibe una lista de turnos en los cuales disparar y los convierte en una partida valida"""
+		partidas = []
+		for combinacion in combinaciones:
+			partida = []
+			for turno in combinacion:
+				while len(partida) <= turno: partida += [None]
+				partida[turno] = [idBarco] + [None] * (cantidadLanzaderas - 1)
+			partidas.append(partida)
+		return partidas
 
 	def jugarPartidaMatrizFinita(self, resultados, barcos):
-		primeraPartida = self.partidaOptima(resultados)
 		barcosAtacados = []
-		for sublist in primeraPartida:
+		for sublist in resultados:
 			if not sublist: continue
 			for item in sublist:
 				if item != None and item not in barcosAtacados:
@@ -137,54 +135,54 @@ class Dyno(Jugador):
 			barcosRemanentes = sorted([b for b in barcos if b.getID() not in barcosAtacados],key=lambda b:-b.getVida())
 		else:
 			barcosRemanentes = sorted([b for b in barcos if b.getID()],key=lambda b:-b.getVida())
-		return primeraPartida, barcosRemanentes
+		return resultados, barcosRemanentes
 
+	def jugarPartidaNueva(self, partidaVieja, partidaAgregada, cantidadLanzaderas):
+		"""Teniendo en cuenta una partida parcialmente jugada para un barco (partidaVieja), agrega una segunda partida (partidaAgregada) que mata al nuevo barco"""
+		for turnoParcial, turnoActual in zip(partidaVieja, partidaAgregada):
+			# Si no son compatibles las partidas, la descarta
+			if turnoActual and turnoParcial and all([x for x in turnoParcial if x != 0]): return False
+		partidaNueva = []
+		for turnoParcial, turnoActual in zip(partidaVieja, partidaAgregada):
+			turnoParcialValido = [x for x in turnoParcial if x != None] if turnoParcial else []
+			turnoActualValido = [x for x in turnoActual if x != None] if turnoActual else []
+			if len(turnoParcialValido) + len(turnoActualValido) <= cantidadLanzaderas:
+				turnoNuevo = turnoParcialValido + turnoActualValido
+				partidaNueva.append(turnoNuevo + [None] * (cantidadLanzaderas - len(turnoNuevo)))
+		partidaNueva += partidaVieja[len(partidaNueva):]
+		partidaNueva += partidaAgregada[len(partidaNueva):]
+		return partidaNueva
 
-def sortPorBarcoDificilDeMatar(turnosParaTodos):
-	heapBarcosDificiles = []
-	for barco in turnosParaTodos:
-		cantPosiblesCombinaciones = len(turnosParaTodos[barco])
-		heappush(heapBarcosDificiles, (cantPosiblesCombinaciones, barco.getID()))
-	return heapBarcosDificiles
+	def sortPorBarcoDificilDeMatar(self,turnosParaTodos):
+		"""El barco mas dificil de matar es el que menos partidas que lo mate tenga"""
+		heapBarcosDificiles = []
+		for barco in turnosParaTodos:
+			cantidadPartidasPosiblesQueLoMatan = len(turnosParaTodos[barco])
+			heappush(heapBarcosDificiles, (cantidadPartidasPosiblesQueLoMatan, barco.getID()))
+		return heapBarcosDificiles
 
-
-def simularPartidaConPartidaParcial(partidaParcial, partidaBarcoActual, cantidadLanzaderas):
-	for turnoParcial, turnoActual in zip(partidaParcial, partidaBarcoActual):
-		if turnoActual and turnoParcial and all([x for x in turnoParcial if x != 0]): return False
-	partidaResultado = []
-	for turnoParcial, turnoActual in zip(partidaParcial, partidaBarcoActual):
-		turnoParcialValido = [x for x in turnoParcial if x != None] if turnoParcial else []
-		turnoActualValido = [x for x in turnoActual if x != None] if turnoActual else []
-		if len(turnoParcialValido) + len(turnoActualValido) <= cantidadLanzaderas:
-			turnoNuevo = turnoParcialValido + turnoActualValido
-			partidaResultado.append(turnoNuevo + [None] * (cantidadLanzaderas - len(turnoNuevo)))
-	partidaResultado += partidaParcial[len(partidaResultado):]
-	partidaResultado += partidaBarcoActual[len(partidaResultado):]
-	return partidaResultado
-
-
-def matarBarcosRemanentes(partida, barcosRemanentes , matriz, cantidadLanzaderas):
-	"""De manera greedy, mata a todos los barcos que no pudo matar en una partida con la matriz finita
-	Primero llena todas las lanzaderas sin disparar con el primer barco remanente"""
-	partidaNueva = []
-	for barco in barcosRemanentes:
-		vida = barco.getVida()
-		id = barco.getID()
-		for i, turno in enumerate(partida):
-			turnoNuevo = copy(turno)
-			if turno and all([x for x in turno if x != 0]): pass
-			elif not turno:
-				turnoNuevo = [barco.getID()] * (cantidadLanzaderas)
+	def matarBarcosRemanentes(self,partida, barcosRemanentes , matriz, cantidadLanzaderas):
+		"""De manera greedy, mata a todos los barcos que no pudo matar en una partida con la matriz finita
+		Primero llena todas las lanzaderas sin disparar con el primer barco remanente"""
+		partidaNueva = []
+		for barco in barcosRemanentes:
+			vida = barco.getVida()
+			id = barco.getID()
+			for i, turno in enumerate(partida):
+				turnoNuevo = copy(turno)
+				if turno and all([x for x in turno if x != 0]): pass
+				elif not turno:
+					turnoNuevo = [barco.getID()] * (cantidadLanzaderas)
+					vida -= cantidadLanzaderas * matriz[id][i]
+				else:
+					for i, lanzadera in enumerate(turno):
+						if lanzadera == None:
+							turnoNuevo[i] = id
+							vida -= matriz[id][i]
+				partidaNueva.append(turnoNuevo)
+				if vida <= 0: break
+			while (vida > 0):
+				partidaNueva.append([barco.getID()] * (cantidadLanzaderas))
 				vida -= cantidadLanzaderas * matriz[id][i]
-			else:
-				for i, lanzadera in enumerate(turno):
-					if lanzadera == None:
-						turnoNuevo[i] = id
-						vida -= matriz[id][i]
-			partidaNueva.append(turnoNuevo)
-			if vida <= 0: break
-		while (vida > 0):
 			partidaNueva.append([barco.getID()] * (cantidadLanzaderas))
-			vida -= cantidadLanzaderas * matriz[id][i]
-		partidaNueva.append([barco.getID()] * (cantidadLanzaderas))
-	return partidaNueva
+		return partidaNueva
